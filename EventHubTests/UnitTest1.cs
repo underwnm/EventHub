b3f6e687d28c444d7a039557066666f44b060b03
Msatkin <mscottaa@gmail.com>
2017-01-03 14:21:10 -0600

    Added Test Project

--- EventHubTests/UnitTest1.cs
+++ EventHubTests/UnitTest1.cs
@@ -7,8 +7,9 @@ namespace EventHubTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void RegionTests()
         {
+
         }
     }
 }
